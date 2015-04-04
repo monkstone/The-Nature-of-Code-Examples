653000a26e23d0876392ed42eb829e255b0f06f4
monkstone <martin_p@lineone.net>
2015-04-04 13:23:49 +0100

    favour forwardable when appropriate

--- chp5_physicslibraries/toxiclibs/simple_spring/particle.rb
+++ chp5_physicslibraries/toxiclibs/simple_spring/particle.rb
@@ -1,3 +1,5 @@
+require 'forwardable'
+
 # The Nature of Code
 # Daniel Shiffman
 # http://natureofcode.com
@@ -5,9 +7,11 @@
 # We could have just stored a reference to a VerletParticle2D object
 # inside the Particle class, but inheritance is an alternative
 class Particle < Physics::VerletParticle2D
-  include Processing::Proxy
+  extend Forwardable
+  def_delegators(:@app, :fill, :stroke, :stroke_weight, :ellipse)
   def initialize(loc)
     super(loc)
+    @app = $app
   end
 
   # All we're doing really is adding a display function to a VerletParticle
