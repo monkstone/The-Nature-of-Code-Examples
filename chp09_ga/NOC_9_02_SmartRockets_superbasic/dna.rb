d318ad3e5d1c6d8add87283c0331eab3d7097e49
monkstone <martin_p@lineone.net>
2015-06-25 19:36:22 +0100

    super basic

---
+++ chp09_ga/NOC_9_02_SmartRockets_superbasic/dna.rb
@@ -0,0 +1,50 @@
+# The Nature of Code
+# Daniel Shiffman
+# http://natureofcode.com
+
+# DNA is an array of vectors
+class DNA
+  # The maximum strength of the forces
+  MAX_FORCE = 0.1
+
+  attr_reader :genes, :lifetime
+
+  # Constructor (makes a DNA of random PVectors)
+  def create_genes(lftm)
+    @genes = (0...lftm).map do
+      angle = rand(Math::PI * 2)
+      Vec2D.new(Math.cos(angle), Math.sin(angle)) * rand(0..MAX_FORCE)
+    end
+  end
+
+  # Constructor #2, creates the instance based on an existing array
+  def initialize(lifetime, newgenes = nil)
+    @lifetime = lifetime
+    # We could make a copy if necessary
+    # genes = (PVector []) newgenes.clone();
+    @genes = newgenes.nil? ? create_genes(lifetime) : newgenes.clone
+  end
+
+  # CROSSOVER
+  # Creates new DNA sequence from two (this & and a partner)
+  def crossover(partner)
+    child = Array.new(genes.length, Vec2D.new)
+    # Pick a midpoint
+    crossover = rand(genes.length)
+    # Take "half" from one and "half" from the other
+    genes.length.times do |i|
+      child[i] = (i > crossover) ? genes[i] : partner.genes[i]
+    end
+    DNA.new(lifetime, child)
+  end
+
+  # Based on a mutation probability, picks a new random Vector
+  def mutate(m)
+    genes.length.times do |i|
+      if rand < m
+        angle = rand(Math::PI * 2)
+        genes[i] = Vec2D.new(Math.cos(angle), Math.sin(angle)) * rand(0..MAX_FORCE)
+      end
+    end
+  end
+end
